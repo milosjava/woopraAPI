308caa0432f9f74df9cae50edf4cf01862653430
milos <milosjava@gmail.com>
2015-11-19 16:56:34 +0100

    fix parsing

--- src/main/scala/partners/harris/woopraAPI/Visitors.scala
+++ src/main/scala/partners/harris/woopraAPI/Visitors.scala
@@ -15,11 +15,6 @@ object Visitors extends LazyLogging{
 
 
 
-  //case class for storing
-  case class Visitor(continent: String, date: String, country: String,city : String, pid: String,properties : String)
-
-
-
   /**
     * reads json file
     * @return array of raw json strings
@@ -55,29 +50,41 @@ object Visitors extends LazyLogging{
 
   }
 
-  implicit val documentReader: Reads[Visitor] = (
-    (__ \ "continent").read[String] and
-      (__ \ "country").read[String] and
-      (__ \ "date").read[String] and
-      (__ \ "city").read[String] and
+  //case class for storing json data
+  case class Action( duration: Int, name: String, properties_dn : String, properties_permalink:String)
+
+  //case class for storing json data
+  case class Visitor( date: String, pid: String, name : String, fingerprint:String, actions : List[Action])
+
+
+
+  implicit val actionReader: Reads[Action] = (
+    (__ \ "duration").read[Int] and
+      ((__ \ "name").read[String] or Reads.pure("")) and
+      (__ \ "properties" \ "dn").read[String] and
+      ((__ \ "properties" \ "permalink").read[String] or Reads.pure(""))
+    ) (Action.apply _)
+
+  implicit val visitorReader: Reads[Visitor] = (
+    (__ \ "date").read[String] and
       (__ \ "pid").read[String] and
-      (__ \ "properties").read[String]
+      ((__ \ "visitor" \ "name").read[String] or Reads.pure("")) and
+      ((__ \ "visitor" \ "fingerprint").read[String] or Reads.pure("")) and
+      (__ \ "actions").read[List[Action]]
     ) (Visitor.apply _)
-
-
 
 
   def main(args: Array[String]) {
 
-    //var content = readFile("harrisPartners_2015-11-17_2.log")
+    var content = readFile("harrisPartners_2015-11-17_2.log")
 
-    var row = """{"continent":"EU","date":"2015-11-18","country":"RS","city":"Pančevo","timezone":"Europe/Belgrade","screen":"1920x1080","description":"","language":"English","pid":"ggTQYfnwkMYG","type":"visit","resolution":"1920x1080","second":0,"duration":444,"number":5,"post":"","browser":"Chrome","lat":44.8708,"hour_of_day":2,"os":"Linux","lng":20.6403,"offset":"+01:00","method":"","org":"","ip":"89.216.152.115","h":"ggTQYfnwkMYG","minute":21,"referrer":{"query":"","type":"direct","url":""},"time":1447773660085,"region":"Vojvodina","visitor":{"name":"Milos","fingerprint":"2798630358","first_visit_time":"1447678760470","first_referrer_type":"direct"},"device":"desktop","actions":[{"duration":432,"date":"2:21:12 AM","domain":"harris.partners","name":"wp article","time":1447773672068,"properties":{"author":"Tim Doyle","name":"wp article","dn":"432","permalink":"http://harris.partners/the-challenge-of-owning-moments/","title":"The challenge of owning moments","category":"Blog","post date":"1443532696000"}},{"duration":10,"date":"2:21:04 AM","exit":true,"domain":"harris.partners","name":"pv","time":1447773664139,"properties":{"domain":"harris.partners","name":"pv","dn":"10","title":"\n\nThought Leadership - Harris Partners | We are revolutionising customer engagement through digital &amp; big data.","uri":"http://harris.partners/thought-leadership/","url":"/thought-leadership/"}},{"duration":10,"date":"2:21:00 AM","landing":true,"domain":"harris.partners","name":"pv","time":1447773660085,"properties":{"domain":"harris.partners","name":"pv","dn":"10","title":"\n\nHarris Partners | Home Page | We are revolutionising customer engagement through digital &amp; big data.","uri":"http://harris.partners/","url":"/"}}]}
-                    """
-
-    var content = Array[String]()
-
-
-    content = content :+ row
+//    var row = """{"continent":"EU","date":"2015-11-18","country":"RS","city":"Pančevo","timezone":"Europe/Belgrade","screen":"1920x1080","description":"","language":"English","pid":"ggTQYfnwkMYG","type":"visit","resolution":"1920x1080","second":0,"duration":444,"number":5,"post":"","browser":"Chrome","lat":44.8708,"hour_of_day":2,"os":"Linux","lng":20.6403,"offset":"+01:00","method":"","org":"","ip":"89.216.152.115","h":"ggTQYfnwkMYG","minute":21,"referrer":{"query":"","type":"direct","url":""},"time":1447773660085,"region":"Vojvodina","visitor":{"name":"Milos","fingerprint":"2798630358","first_visit_time":"1447678760470","first_referrer_type":"direct"},"device":"desktop","actions":[{"duration":432,"date":"2:21:12 AM","domain":"harris.partners","name":"wp article","time":1447773672068,"properties":{"author":"Tim Doyle","name":"wp article","dn":"432","permalink":"http://harris.partners/the-challenge-of-owning-moments/","title":"The challenge of owning moments","category":"Blog","post date":"1443532696000"}},{"duration":10,"date":"2:21:04 AM","exit":true,"domain":"harris.partners","name":"pv","time":1447773664139,"properties":{"domain":"harris.partners","name":"pv","dn":"10","title":"\n\nThought Leadership - Harris Partners | We are revolutionising customer engagement through digital &amp; big data.","uri":"http://harris.partners/thought-leadership/","url":"/thought-leadership/"}},{"duration":10,"date":"2:21:00 AM","landing":true,"domain":"harris.partners","name":"pv","time":1447773660085,"properties":{"domain":"harris.partners","name":"pv","dn":"10","title":"\n\nHarris Partners | Home Page | We are revolutionising customer engagement through digital &amp; big data.","uri":"http://harris.partners/","url":"/"}}]}
+//              """
+//
+//    var content = Array[String]()
+//
+//
+//    content = content :+ row
 
     val entries = parse(content)
 
--- src/test/scala/partners/harris/woopraAPI/VisitorsSpec.scala
+++ src/test/scala/partners/harris/woopraAPI/VisitorsSpec.scala
@@ -7,11 +7,46 @@ import org.scalatest.FunSuite
   */
 class VisitorsSpec extends FunSuite{
 
-  test("loading file"){
+  test("loading json from file"){
 
     val content = Visitors.readFile("two-days-ago.log")
 
     assert(content.size == 11)
+
+  }
+
+
+  test("parse json "){
+
+
+    //test data for parsing
+    val row = """{"continent":"EU","date":"2015-11-18","country":"RS","city":"Pančevo","timezone":"Europe/Belgrade","screen":"1920x1080","description":"","language":"English","pid":"ggTQYfnwkMYG","type":"visit","resolution":"1920x1080","second":0,"duration":444,"number":5,"post":"","browser":"Chrome","lat":44.8708,"hour_of_day":2,"os":"Linux","lng":20.6403,"offset":"+01:00","method":"","org":"","ip":"89.216.152.115","h":"ggTQYfnwkMYG","minute":21,"referrer":{"query":"","type":"direct","url":""},"time":1447773660085,"region":"Vojvodina","visitor":{"name":"Milos","fingerprint":"2798630358","first_visit_time":"1447678760470","first_referrer_type":"direct"},"device":"desktop","actions":[{"duration":432,"date":"2:21:12 AM","domain":"harris.partners","name":"wp article","time":1447773672068,"properties":{"author":"Tim Doyle","name":"wp article","dn":"432","permalink":"http://harris.partners/the-challenge-of-owning-moments/","title":"The challenge of owning moments","category":"Blog","post date":"1443532696000"}},{"duration":10,"date":"2:21:04 AM","exit":true,"domain":"harris.partners","name":"pv","time":1447773664139,"properties":{"domain":"harris.partners","name":"pv","dn":"10","title":"\n\nThought Leadership - Harris Partners | We are revolutionising customer engagement through digital &amp; big data.","uri":"http://harris.partners/thought-leadership/","url":"/thought-leadership/"}},{"duration":10,"date":"2:21:00 AM","landing":true,"domain":"harris.partners","name":"pv","time":1447773660085,"properties":{"domain":"harris.partners","name":"pv","dn":"10","title":"\n\nHarris Partners | Home Page | We are revolutionising customer engagement through digital &amp; big data.","uri":"http://harris.partners/","url":"/"}}]}
+              """
+
+    var content = Array[String]()
+
+
+    content = content :+ row
+
+    val entries = Visitors.parse(content)
+
+    //check if it returns one
+    assert(entries.length == 1)
+
+    //check name is Milos
+    assert(entries(0).name == "Milos")
+
+    //TODO pid wil be probably removed
+    //check pid is ggTQYfnwkMYG
+    assert(entries(0).pid == "ggTQYfnwkMYG")
+
+    //check fingerprint is 2798630358
+    assert(entries(0).fingerprint == "2798630358")
+
+    val actions = entries(0).actions
+
+    assert(actions.length ==2 )
+
 
 
   }
